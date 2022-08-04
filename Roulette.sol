485de2af76fc599082d36ba85fecba8ef0cc7b7d
Remix Workspace <>
2022-08-04 10:35:32 +0200

    Implements private unsafe roulette spin function for a target block nubmer

--- Roulette.sol
+++ Roulette.sol
@@ -18,4 +18,14 @@ contract Roulette {
         payable(msg.sender).transfer(_amount);
     }
 
+    function _unsafeRouletteSpin(uint blockNum) private view returns (uint8) {
+        // bytes32 unsafeHashInput = blockhash(blockNum) ^ bytes32(block.timestamp + block.number);
+        // `blockhash` does not work with Remix VM, should work with a proper testnet, although with limitations:
+        // https://docs.soliditylang.org/en/latest/units-and-global-variables.html#block-and-transaction-properties
+        // For now, add `blockNum * 232931707` (a randomly chosed prime number).
+        bytes32 unsafeHashInput = bytes32(block.timestamp + block.number + blockNum * 232931707);
+        bytes32 hash = keccak256(abi.encodePacked(unsafeHashInput));
+        return uint8(uint(hash)) % 37;
+    }
+
 }
