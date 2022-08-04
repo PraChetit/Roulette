7d73f5d74bb797ea61da55c7540aa17bbb5ca063
Remix Workspace <>
2022-08-04 13:15:02 +0200

    Initial logic for realizing bets after the target block becomes available

--- Roulette.sol
+++ Roulette.sol
@@ -6,19 +6,19 @@ pragma solidity ^0.8.15;
 contract Roulette {
 
     enum BetType {
-        Black,
-        Red
+        Even,
+        Odd
     }
 
     struct Bet {
-        address bettor;
         BetType betType;
         uint amount;
         uint blockNum;
+        bool pending;
     }
 
     address payable public owner;
-    mapping(address => Bet[]) public unrealizedBets;
+    mapping(address => Bet) public allBets;
     uint public totalBalance;
     uint public freeBalance;
 
@@ -33,35 +33,61 @@ contract Roulette {
         freeBalance += msg.value;
     }
 
-    function betOnBlack(uint futureBlockNumber) external payable {
-        _betOnColor(futureBlockNumber, BetType.Black);
+    function betOnEven(uint futureBlockNumber) external payable {
+        _betOnParity(futureBlockNumber, BetType.Even);
     }
 
-    function betOnRed(uint futureBlockNumber) external payable {
-        _betOnColor(futureBlockNumber, BetType.Red);
+    function betOnOdd(uint futureBlockNumber) external payable {
+        _betOnParity(futureBlockNumber, BetType.Odd);
     }
 
-    function _betOnColor(uint futureBlockNumber, BetType betType) private {
+    function _betOnParity(uint futureBlockNumber, BetType betType) private {
         require(msg.value > 0, "Must bet a positive amount");
         require(msg.value < freeBalance, "Bank has insufficient funds to support this bet.");
         require(futureBlockNumber > block.number, "Bet must be placed in the future.");
+        require(!allBets[msg.sender].pending, "Bettor already has a pending bet.");
         freeBalance -= msg.value;
         totalBalance += msg.value;
-        Bet memory newBet = Bet(msg.sender, betType, msg.value, futureBlockNumber);
-        unrealizedBets[msg.sender].push(newBet);
+        allBets[msg.sender] = Bet(betType, msg.value, futureBlockNumber, true);
+    }
+
+    function realizeMyBet() external {
+        Bet storage myBet = allBets[msg.sender];
+        require(myBet.pending, "No pending bet for bettor.");
+        require(myBet.blockNum < block.number, "Target block number does not exist yet.");
+
+        uint8 spin = _unsafeRouletteSpin(myBet.blockNum) % 37;
+        bool win;
+        if (spin == 0) {
+            // Zero is on the house.
+            win = false;
+        } else {
+            win = (myBet.betType == BetType.Even) ? (spin % 2) == 0 : (spin % 2) == 1;
+        }
+
+        if (win) {
+            totalBalance -= 2 * myBet.amount;
+            payable(msg.sender).transfer(2 * myBet.amount);
+        } else {
+            freeBalance += 2 * myBet.amount;
+        }
+
+        myBet.pending = false;
     }
 
     function withdraw(uint amount) external {
         require(msg.sender == owner, "Caller is not the owner.");
         require(amount <= freeBalance, "Not enough free balance to support the requested withdrawal.");
+        freeBalance -= amount;
+        totalBalance -= amount;
         payable(msg.sender).transfer(amount);
     }
 
     function _unsafeRouletteSpin(uint blockNum) private view returns (uint8) {
-        // bytes32 unsafeHashInput = blockhash(blockNum) ^ bytes32(block.timestamp + block.number);
+        // bytes32 unsafeHashInput = blockhash(blockNum);
         // `blockhash` does not work with Remix VM, should work with a proper testnet, although with limitations:
         // https://docs.soliditylang.org/en/latest/units-and-global-variables.html#block-and-transaction-properties
-        // For now, add `blockNum * 232931707` (a randomly chosed prime number).
+        // For now, use timestample and add `blockNum * 232931707` (a randomly chosed prime number).
         bytes32 unsafeHashInput = bytes32(block.timestamp + block.number + blockNum * 232931707);
         bytes32 hash = keccak256(abi.encodePacked(unsafeHashInput));
         return uint8(uint(hash)) % 37;
